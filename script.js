e79f0a28c11eb9af08f2c6b2bca8ae7d7b830d88
Meynrun <ahakahacker@mail.ru>
2024-11-15 18:37:14 +0300

    update

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
-        }
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            } else {
+                entry.target.classList.remove("show");
+            }
+        });
     });
-});
 
-const hiddenElements = document.getElementsByClassName("hide");
-hiddenElements.forEach((el) => observer.observe(el));
+    const hiddenElements = document.getElementsByClassName("hide");
+    Array.from(hiddenElements).forEach((el) => observer.observe(el));
+});
